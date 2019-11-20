582f7db6b8546a0b80402a3baf81f6a69b22e1f9
rtealw <r.teal.witter@gmail.com>
2019-11-19 21:59:40 -0500

    ready to simulate!

--- simulate_wrapper.py
+++ simulate_wrapper.py
@@ -6,16 +6,40 @@ def simulate_iterations(iterations=100, output="output/games.txt", agent_names =
     for iteration in range(iterations):
         reload(simulate)
         results = simulate.run_game(agent_names=agent_names, point_table=point_table)
+        results['point_table'] = point_table
         output_file.write(str(results) + '\n')
-        for winner in results['winners']:
-            winner_name = agent_names[winner]
-            if winner_name not in wins:
-                wins[winner_name] = 0
-            wins[winner_name] += 1
-            wins["total"] += 1
-        current_prop = float(wins['LongRouteJunkie'])/wins["total"]
-        print "Iteration:", iteration, "Long Route Proportion", current_prop
+#        for winner in results['winners']:
+#            winner_name = agent_names[winner]
+#            if winner_name not in wins:
+#                wins[winner_name] = 0
+#            wins[winner_name] += 1
+#            wins["total"] += 1
+#        current_prop = float(wins['LongRouteJunkie'])/wins["total"]
+#        print "Iteration:", iteration, "Long Route Proportion", current_prop
+#    print(wins)
     output_file.close()
-    print(wins)
 
-simulate_iterations(iterations=20)
+def get_linear_point_table(alpha):
+    point_table = {}
+    for k in range(1, 7):
+        point_table[k] = alpha * k
+    return point_table
+
+def float_range(start, stop, step):
+    result = []
+    for i in range(int((stop-start+step)/step)):
+        result += [start + step * i]
+    return result
+
+def simulate_point_tables(iterations):
+    for alpha in float_range(start=1, stop=2.5, step=.1):
+        point_table = get_linear_point_table(alpha)
+        print(point_table)
+        simulate_iterations(
+            iterations=iterations, 
+            output="output/point_tables.txt",
+            point_table=point_table
+        )
+
+simulate_iterations(iterations=10000)
+simulate_point_tables(iterations=1000)
